218690c584fbe70ffdfccd3d7e12afc3341cd1db
Fadi S.Neirat <fadi.nairat.2010@gmail.com>
2024-10-22 13:05:01 -0500

    PresentationSubmitPres

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
     title: "Cloud Computing Project",
     itemsPerPage: 8,
     host: "http://localhost:5000",
-    serviceHost: "https://cnerpmmvn0.execute-api.us-east-1.amazonaws.com/prod/users",
+    serviceHost: "https://nmj5pl8spb.execute-api.us-east-1.amazonaws.com/prod/users",
     token_key: "auth_token",
     registerMsg: "You have been registered successfully...",
     registerFailed: "Registration Failed!!!"
